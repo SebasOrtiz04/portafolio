40ea812e89873066b9b96df871353522b61a52e3
Juan Sebastian Ortiz Castro <93163636+SebasOrtiz04@users.noreply.github.com>
2024-02-23 21:43:13 -0600

    snv config

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -5,9 +5,9 @@ const nextConfig = {
     formats:['image/avif','image/webp'],
     // domains:['aca el dominio de la api']
   },
-  env: {
-    NEXT_URL_BASE_API: 'http://127.0.0.1:8000/api/',
-  },
+  // env: {
+  //   NEXT_URL_BASE_API: 'http://127.0.0.1:8000/api/',
+  // },
 };
 
 export default nextConfig
